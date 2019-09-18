9def755090832ae3474dce5d93212319ceb307f0
Roger Wang <rogerwangcs@gmail.com>
2019-09-18 14:21:28 -0400

    Updates

---
+++ precache-manifest.9ed7cc091c0aa410fe4a01fbddb2210b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecb932f5f5921279384dc3bf095859f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c6de6ba235bfb41e598",
+    "url": "/static/css/main.ca7c6434.chunk.css"
+  },
+  {
+    "revision": "f69141606a87a08f8176",
+    "url": "/static/js/2.72327702.chunk.js"
+  },
+  {
+    "revision": "0c6de6ba235bfb41e598",
+    "url": "/static/js/main.11a0507b.chunk.js"
+  },
+  {
+    "revision": "212597167309cc210b04",
+    "url": "/static/js/runtime~main.2fa4ff05.js"
+  },
+  {
+    "revision": "80f944c68504c90843732b9910da15b9",
+    "url": "/static/media/Profile-Picture.80f944c6.png"
+  },
+  {
+    "revision": "35602579708e76a9d583d2ea7b15e859",
+    "url": "/static/media/ResumeGraphic.35602579.png"
+  },
+  {
+    "revision": "8c414ccfa84a788b2aed163bdc8ea7dd",
+    "url": "/static/media/better-bc-housing.8c414ccf.PNG"
+  },
+  {
+    "revision": "abd3303b387dcefff9206845995fd045",
+    "url": "/static/media/live-subtitles.abd3303b.png"
+  },
+  {
+    "revision": "508083a86416ccf963577cd12d2ed144",
+    "url": "/static/media/rogersblog1.508083a8.jpg"
+  },
+  {
+    "revision": "138de344eea46c41d7ed96a042637daf",
+    "url": "/static/media/writefast1.138de344.jpg"
+  }
+]);
